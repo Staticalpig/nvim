7cfb3dc509601e0894b8ac52088902b9c79cb7de
StaticalPig <elinus@live.se>
2026-03-03 19:29:43 +0100

    Added Harpoon 2

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,38 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--- HARPOON 2 MAPS
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function()
+  harpoon:list():add()
+end)
+vim.keymap.set("n", "<C-e>", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+vim.keymap.set("n", "<C-n>", function()
+  harpoon:list():select(1)
+end)
+vim.keymap.set("n", "<C-m>", function()
+  harpoon:list():select(2)
+end)
+vim.keymap.set("n", "<C-,>", function()
+  harpoon:list():select(3)
+end)
+vim.keymap.set("n", "<C-.>", function()
+  harpoon:list():select(4)
+end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function()
+  harpoon:list():prev()
+end)
+vim.keymap.set("n", "<C-S-N>", function()
+  harpoon:list():next()
+end)
