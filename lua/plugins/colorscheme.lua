d87af5a572a7ca298940a5fceaefd15d34fc0856
StaticalPig <elinus@live.se>
2026-03-03 18:45:01 +0100

    Fixed weird loading of theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,25 @@
 return {
   { "catppuccin/nvim", name = "catppuccin" },
-  { "projekt0n/github-nvim-theme", name = "github-theme" },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({
+        -- your config here
+      })
+      -- You can keep this, but the LazyVim opts below will usually override it
+      vim.cmd("colorscheme github_dark")
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github-theme",
+      -- CHANGE THIS: "github-theme" is the plugin name.
+      -- Use "github_dark" or "github_light" for the actual colorscheme.
+      colorscheme = "github_dark",
     },
   },
 }
