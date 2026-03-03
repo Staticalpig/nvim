eb0653948a4479b288c2d110872131768f5e8ede
StaticalPig <elinus@live.se>
2026-03-03 18:33:51 +0100

    Added github-theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "projekt0n/github-nvim-theme", name = "github-theme" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "github-theme",
+    },
+  },
+}
